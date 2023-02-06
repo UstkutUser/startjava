636da5dc53a44de01a133da89a97bac823646345
UstkutUser <ershov2007@gmail.com>
2023-02-06 22:19:31 +0700

    homework

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,18 +1,22 @@
-public class Person{
+public class Person {
     String gender = "male";
     String name = "Gena";
     int heigth = 201;
     int weigth = 95;
     int age = 40;
 
-    public void move(){
-    };
-    public void sit(){
-    };
-    public void run(){
-    };
-    public void speak(){
-    };
-    public void studyJava(){
-    };
+    public void move() {
+    }
+
+    public void sit() {
+    }
+
+    public void run() {
+    }
+
+    public void speak() {
+    }
+    
+    public void studyJava() {
+    }
 }
