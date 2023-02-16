37a0d0bbaf8b27e80ce182490bbe5c674e709281
UstkutUser <ershov2007@gmail.com>
2023-02-16 22:34:44 +0700

    homework

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,9 +1,52 @@
 public class Wolf {
-    String gender;
-    String nickname;
-    int weight;
-    int age;
-    String color;
+    private String gender = "male";
+    private String nickname = "Wolfenstein";
+    private int weight = 45;
+    private int age = 4;
+    private String color = "grey";
+
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 
     public void move() {
         System.out.println("Ходит");
@@ -20,7 +63,7 @@ public class Wolf {
     public void howl() {
         System.out.println("Воет");
     }
-    
+
     public void hunt() {
         System.out.println("Охотится");
     }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,19 +1,18 @@
 public class WolfTest {
-
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        
-        String gender = "male";
-        String nickname = "Wolfenstein";
-        int weight = 45;
-        int age = 4;
-        String color = "grey";
 
-        System.out.println("gender of wolf: " + gender);
-        System.out.println("nickname of wolf: " + nickname);
-        System.out.println("weight of wolf: " + weight);
-        System.out.println("age of wolf: " + age);
-        System.out.println("color of wolf: " + color);
+        wolfOne.setGender("female");
+        wolfOne.setNickname("Wolfy");
+        wolfOne.setWeight(38);
+        wolfOne.setAge(3);
+        wolfOne.setColor("white");
+
+        System.out.println("gender of wolf: " + wolfOne.getGender());
+        System.out.println("nickname of wolf: " + wolfOne.getNickname());
+        System.out.println("weight of wolf: " + wolfOne.getWeight());
+        System.out.println("age of wolf: " + wolfOne.getAge());
+        System.out.println("color of wolf: " + wolfOne.getColor());
 
         wolfOne.move();
         wolfOne.sit();
