d0a87b611fc4abf8c408dc45d72635f7359aa98a
UstkutUser <ershov2007@gmail.com>
2023-03-08 13:30:07 +0700

    homework

--- Lesson_2/Jaeger.java
+++ Lesson_2/Jaeger.java
@@ -6,10 +6,8 @@ public class Jaeger {
     private float weight;
     private int speed;
 
-    public Jaeger() {
-        super();
-    }
-    // parameterized  constrictor
+    public Jaeger() {}
+    // parameterized constructor
     public Jaeger(String modelName, String name, String origin, float height, float weight, int speed) {
         this.modelName = modelName;
         this.name = name;
@@ -18,33 +16,43 @@ public class Jaeger {
         this.weight = weight;
         this.speed = speed;
     }
-    public void move() {
-        System.out.println(name + " moved forward to fight");
-    }
-    public void userArmor() {
-        System.out.println(name + " applied super strong armor");
-    }
-    public void protect() {
-        System.out.println(name + " will protect the base.");
-    }
+
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public void setOrigin(String origin) {
         this.origin = origin;
     }
+
     public void setHeight(float height) {
         this.height = height;
     }
+
     public void setWeight(float weight) {
         this.weight = weight;
     }
+
     public void setSpeed(int speed) {
         this.speed = speed;
     }
+
+    public void move() {
+        System.out.println(name + " moved forward to fight");
+    }
+
+    public void userArmor() {
+        System.out.println(name + " applied super strong armor");
+    }
+
+    public void protect() {
+        System.out.println(name + " will protect the base.");
+    }    
+
     public void print() {
         System.out.println("Name: " + name + " modelName: " + modelName + " origin: " + origin + 
                 "\n" + "height: " + height + " weight: " + weight + " speed: " + speed);
--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,22 +1,34 @@
 public class JaegerTest {
 
     public static void main(String[] args) {
-        Jaeger HorizonBrave = new Jaeger("Mark-1", "HorizonBrave", "China", 72.54f, 7.890f, 8);
-        HorizonBrave.print();
-        HorizonBrave.move();
-        HorizonBrave.userArmor();
-        HorizonBrave.protect();
-        Jaeger CoyoteTango = new Jaeger();
-        CoyoteTango.setModelName("Mark-1");
-        CoyoteTango.setName("Coyote Tango");
-        CoyoteTango.setOrigin("Japan");
-        CoyoteTango.setHeight(84.0f);
-        CoyoteTango.setWeight(2312.0f);
-        CoyoteTango.setSpeed(8);
+        Jaeger horizonBrave = new Jaeger("Mark-1", "HorizonBrave", "China", 72.54f, 7.890f, 8);
+        horizonBrave.print();
+        horizonBrave.move();
+        horizonBrave.userArmor();
+        horizonBrave.protect();
+        
+        Jaeger coyoteTango = new Jaeger();
+
+        coyoteTango.setModelName("Mark-1");
+
+        coyoteTango.setName("Coyote Tango");
+
+        coyoteTango.setOrigin("Japan");
+
+        coyoteTango.setHeight(84.0f);
+
+        coyoteTango.setWeight(2312.0f);
+
+        coyoteTango.setSpeed(8);
+
         System.out.println();
-        CoyoteTango.print();
-        CoyoteTango.move();
-        CoyoteTango.userArmor();
-        CoyoteTango.protect();
+
+        coyoteTango.print();
+
+        coyoteTango.move();
+
+        coyoteTango.userArmor();
+
+        coyoteTango.protect();
     }
 }
