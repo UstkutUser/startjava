b1d419102cad15da1594b87c622630fead56825b
UstkutUser <ershov2007@gmail.com>
2023-03-19 23:28:00 +0700

    homework

--- Lesson_2/GuessNumberTest.java
+++ Lesson_2/GuessNumberTest.java
@@ -1,32 +1,29 @@
 import java.util.Scanner;
-import java.lang.Math;
 
 public class GuessNumberTest {
     public static void main(String[] args) {
         System.out.println("Игра \"Угадай число от 0 до  100\"");
         Scanner console = new Scanner(System.in);
         System.out.print("Введите имя первого игрока: ");
-        String nameOne = console.nextLine();
-        Player playerOne = new Player(nameOne);
+        String name = console.nextLine();
+        Player player1 = new Player(name);
 
         System.out.print("Введите имя второго игрока: ");
-        String nameTwo = console.nextLine();
-        Player playerTwo = new Player(nameTwo);
+        name = console.nextLine();
+        Player player2 = new Player(name);
 
-        GuessNumber guessNumber = new GuessNumber(playerOne, playerTwo);
+        GuessNumber game = new GuessNumber(player1, player2);
 
-        String userAnswer = "yes";
+        String playerAnswer = "yes";
 
         do {
-            if (userAnswer.equals("yes")) {
-                guessNumber.guessNumber();
+            if (playerAnswer.equals("yes")) {
+                game.start();
             }
 
             System.out.print("Хотите продолжить игру? [yes/no]: ");
-            userAnswer = console.nextLine();
-            if (userAnswer.equals("no")) {
-                break;
-            }
-        } while (!userAnswer.equals("no"));
+            playerAnswer = console.nextLine();
+            
+        } while (!playerAnswer.equals("no"));
     }
 }
